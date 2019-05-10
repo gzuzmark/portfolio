cec3f322678c1c3928ff7376756d6d60e01e3716
alonso <alonso.m144@gmail.com>
2019-05-09 22:19:15 -0500

    Initialize project

---
+++ colors.js
@@ -0,0 +1,12 @@
+module.exports = {
+  background: '#FFFFFF',
+  backgroundDark: '#FFFFFF',// '#f0e6f6',
+
+  primary: '#333333',
+  primaryLight: '#ae66df',
+  primaryDark: '#9e15bf', // '#4b007d',
+
+  secondary: '#4ac6d2', // '#ff4081',
+  secondaryLight: '#89dae2',
+  secondaryDark: '#c60055',
+};
