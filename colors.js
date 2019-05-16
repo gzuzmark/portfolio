65eb4b30d8994554ac05512ab111be02f4145933
alonso <alonso.m144@gmail.com>
2019-05-16 16:20:51 -0500

    Change primary light color in order to have contrast with background color.This resolves a11y contrast case

--- colors.js
+++ colors.js
@@ -1,9 +1,9 @@
 module.exports = {
   background: '#FFFFFF',
-  backgroundDark: '#FFFFFF',// '#f0e6f6',
+  backgroundDark: '#f0e6f6',// '#f0e6f6',
 
   primary: '#333333',
-  primaryLight: '#ae66df',
+  primaryLight: '#621E8F',
   primaryDark: '#9e15bf', // '#4b007d',
 
   secondary: '#4ac6d2', // '#ff4081',
